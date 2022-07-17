f64f3f05af6d0100201609ed8951d12a92bdb887
CHJ30 <caihongjian98@gmail.com>
2022-07-17 21:43:43 +0800

    hp 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
   ListItem,
   Divider,
   AppBar,
-  TextField,
   Table,
   TableHead,
   TableRow,
@@ -20,7 +19,6 @@ import {
 // pick a theme of your choice
 import original from "react95/dist/themes/original";
 import avatar from "./assets/images/avatar.jpeg";
-import wx from "./assets/images/wx.jpg";
 // original Windows95 font (optionally)
 import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
 import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
